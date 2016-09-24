8523f22a7c367e5e2628a9dfb215da7a292d1d8c
Kevin Liu <kevinliu314@gmail.com>
2016-09-23 23:17:29 -0500

    draw circle

--- JavaScript/grad_clock.js
+++ JavaScript/grad_clock.js
@@ -1,24 +1,14 @@
-var circle = {
-  'x': 50,
-  'y': 50,
-  'radius': 100,
-  'fill': '#000000'
-};
-var canvas = $('#gradient_canvas').get(0);
-var context = canvas.getContext("2d");
-
-var createGradient = function(){
-  var width = canvas.width;
-  var height = canvas.height;
-  var gradient = context.createLinearGradient(0,0,width,height);
-  gradient.addColorStop(0,"#01020c");
-  gradient.addColorStop(0.33,"#71c9f1");
-  gradient.addColorStop(0.5,"#FFCD05");
-  gradient.addColorStop(0.66,"#336699");
-  gradient.addColorStop(1,"#01020c");
-  context.fillStyle=gradient;
-  context.fillRect(0,0,width,height);
+$(document).ready(function(){
+  createTime();//call oncce to pre render
   var t = setInterval(createTime,500);
+  init();
+});
+
+var circle = {
+  'x': 110,
+  'y': 750,
+  'radius': 100,
+  'fill': 'yellow'
 };
 
 var createTime = function(){
@@ -38,13 +28,37 @@ var checkTime = function(i){
     return i;
 };
 
+var init = function(){
+  var canvas = $('#circle_canvas').get(0);
+  var context = canvas.getContext("2d");
+  canvas.width = $('#circle_canvas').parent().width();
+  canvas.height = $('#circle_canvas').parent().height();
+  var width = canvas.width;
+  var height = canvas.height;
+  // var gradient = context.createLinearGradient(0,0,width,height);
+  // gradient.addColorStop(0,"#01020c");
+  // gradient.addColorStop(0.33,"#71c9f1");
+  // gradient.addColorStop(0.5,"#FFCD05");
+  // gradient.addColorStop(0.66,"#336699");
+  // gradient.addColorStop(1,"#01020c");
+  // context.fillStyle=gradient;
+  // context.fillRect(0,0,width,height);
+  render();
+  //animate('x',0,1000);
+};
+
+
 var render = function(){
+  var canvas = $('#circle_canvas').get(0);
+  var context=canvas.getContext("2d");
+  //draw the canvas circle based on initial start time
   context.clearRect(0,0,canvas.width, canvas.height);
   context.beginPath();
   context.arc(circle.x, circle.y, circle.radius,0,2*Math.PI);
   context.fillStyle = circle.fill;
+  //context.stroke();
   context.fill();
-  requestAnimationFram(render);
+  //requestAnimationFrame(render);
 };
 
 var requestAnimationFrame =
@@ -56,10 +70,17 @@ var requestAnimationFrame =
         function(callback) {
           return setTimeout(callback, 1);
         };
+
 var animate = function(prop, val, duration){
-
+  var start = new Date().getTime();
+  var end = start+duration;
+  var current = circle[prop];
+  var distance = val-current;
+  var step = function(){
+    var timestamp = new Date().getTime();
+    var progress = Math.min((duration-(end-timestamp))/duration, 1);
+    circle[prop] = current + (distance*progress);
+    if(progress<1) requestAnimationFrame(step);
+  };
+  return step();
 };
-
-$(document).ready(function(){
-  createGradient();
-});
